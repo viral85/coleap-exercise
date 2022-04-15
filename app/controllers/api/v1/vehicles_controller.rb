a9785e59a241a92190d2a9896b353e119e225765
Viral Sonawala <viral85@gmail.com>
2022-04-15 17:54:14 +0530

    Added endpoint should allow to sort based on price

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::VehiclesController < ApplicationController
   def index
-    @vehicles = Vehicle.all
+    if params[:sort].present? && ['ASC','DESC'].include?(params[:sort])
+      sort = params[:sort]
+    end
+
+    if sort
+      @vehicles = Vehicle.includes(:vehicle_maker, :vehicle_colors).order(price: params[:sort])
+    else
+      @vehicles = Vehicle.includes(:vehicle_maker, :vehicle_colors).all
+    end
   end
 
   def show
